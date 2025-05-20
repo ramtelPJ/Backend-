eb797e29afcf5796fad4152206fd9b39722cc66c
prajwolramtel <prajwolramtel77@gmail.com>
2025-05-20 12:16:54 -0500

    routing

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,7 @@
-const asyncHandler=(fn)=> (req,res,next)=>{
-    try {
-        
-    } catch (error) {
-        res.status(error.code||500).json({
-            success:false,
-            message:error.message
-        })
+const asyncHandler=(requestHandler)=>{
+    return(req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch
+        ((err)=>next(err))
     }
 }
+export {asyncHandler}
